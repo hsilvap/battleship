032a3728741c98bb6eea5362f48a051ec2090122
Homero Silva <hsilvap@me.com>
2020-10-12 20:18:58 -0500

    Updates

---
+++ precache-manifest.bd069af89539ab479bd014c005e1ce27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c482a6dc1b0deaca63889431b409b502",
+    "url": "/hsilvap/battleship/index.html"
+  },
+  {
+    "revision": "60af9d9850741f64fa21",
+    "url": "/hsilvap/battleship/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f198cb57c67d9caf686",
+    "url": "/hsilvap/battleship/static/js/2.2d6da82e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hsilvap/battleship/static/js/2.2d6da82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60af9d9850741f64fa21",
+    "url": "/hsilvap/battleship/static/js/main.e5119456.chunk.js"
+  },
+  {
+    "revision": "7419ba9d44245ff883b0",
+    "url": "/hsilvap/battleship/static/js/runtime-main.cef70070.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hsilvap/battleship/static/media/logo.5d5d9eef.svg"
+  }
+]);
